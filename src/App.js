6f6cd040203b89acfe0dc80f36c91a8e12feff8e
Brandon E <gunclub_b612@protonmail.ch>
2020-10-24 08:54:31 -0400

    Added interactive markers

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@
 import React from 'react';
 
 import './App.css';
-import SideMap from './react_components/SideMap';
+import SiteMap from './react_components/SiteMap';
 
+const locations = [
+  // Sidney Smith
+  {id: 0, lat: 43.663098, lng: -79.398568},
+  // Bookstore
+  {id: 1, lat: 43.659213, lng: -79.396960},
+  // Isabel Bader
+  {id: 2, lat: 43.667246, lng: -79.392524}
+]
 
 export class App extends React.Component {
-
-  // a 'global' state that you can pass through to any child componenets of App.
-  //   In the Routes below they are passed to both the Home and Queue states.
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapClass: "fullMap",
+      currLocId: -1
+    };
+    this.openLocPage = this.openLocPage.bind(this);
+  }
+  openLocPage(id) {
+    const otherSetting = (this.state.mapClass === "fullMap") ? "sideMap" : "fullMap";
+    if ((this.state.currLocId === -1) || (this.state.currLocId === id)) {
+      this.setState({mapClass: otherSetting, currLocId: ((this.state.currLocId === id) ? -1 : id)});
+    } else {
+      this.setState({currLocId: id});
+    }
+    console.log(this.state);
+  }
   render() {
     return (
       <div>
-        <div className="map">
-          <SideMap />
+        <div className={this.state.mapClass}>
+          <SiteMap locations={locations} openLocPage={this.openLocPage}/>
         </div>
       </div>
       
---
+++ src/react_components/SiteMap/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+
+const mapStyle = {
+  width: '100%',
+  height: '100%'
+};
+
+class SiteMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  generateMarkers() {
+    return this.props.locations.map((loc) => {
+      return <Marker key={loc.id}
+                     id={loc.id}
+                     position={{
+                       lat: loc.lat,
+                       lng: loc.lng
+                     }}
+                     onClick={() => {this.props.openLocPage(loc.id)}}
+              />
+    });
+  }
+
+  render() {
+    return (
+        <Map 
+          google={this.props.google}
+          zoom={16}
+          style={mapStyle}
+          initialCenter={{ lat: 43.663, lng: -79.392}}>
+          {this.generateMarkers()}
+        </Map>
+    );
+  }
+
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAoQit1a6-1wL8EneYqGTO4aFTfmpixG4Y'
+})(SiteMap);
+
